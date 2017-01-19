003db0b3bdcbf19a53b8d9921278f83db9ced664
Jaemin <jaemin0216@gmail.com>
2017-01-19 16:25:40 +0900

    Add AlertView
    - Using UIAlertController
    - Make three UIAlertAction - photoLibraryAction, photoAlbumAction, cancelAction

--- Jaemin/170121/ImagePickerAssignment/ImagePickerAssignment/ViewController.swift
+++ Jaemin/170121/ImagePickerAssignment/ImagePickerAssignment/ViewController.swift
@@ -14,8 +14,34 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.alertView))
+        
+        imageView.addGestureRecognizer(tap)
+        imageView.isUserInteractionEnabled = true
+        
     }
     
+    func alertView() {
+        
+        let controller = UIAlertController(title: "사진 소스 선택", message: "사진을 가져올 소스를 선택해 주세요",
+                                           preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        let photoLibraryAction = UIAlertAction(title: "라이브러리", style: UIAlertActionStyle.default)
+        let photoAlbumAction = UIAlertAction(title: "사진앨범", style: UIAlertActionStyle.default)
+        let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.cancel) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        controller.addAction(photoLibraryAction)
+        controller.addAction(photoAlbumAction)
+        controller.addAction(cancelAction)
+        
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func pickPhotoLibrary() {
+        
+    }
 }
 
