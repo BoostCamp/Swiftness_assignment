188d1fd9797dd22260fc3a2897b97308c88e1c4c
Jaemin <jaemin0216@gmail.com>
2017-01-19 17:10:01 +0900

    Make pickImageFromPhotoLibrary() function
    - Connect to photoLibraryAction
    - Using UIImagePickerController to pick image
    - SourceType is photoLibrary

--- Jaemin/170121/ImagePickerAssignment/ImagePickerAssignment/ViewController.swift
+++ Jaemin/170121/ImagePickerAssignment/ImagePickerAssignment/ViewController.swift
@@ -26,7 +26,9 @@ class ViewController: UIViewController {
         let controller = UIAlertController(title: "사진 소스 선택", message: "사진을 가져올 소스를 선택해 주세요",
                                            preferredStyle: UIAlertControllerStyle.actionSheet)
         
-        let photoLibraryAction = UIAlertAction(title: "라이브러리", style: UIAlertActionStyle.default)
+        let photoLibraryAction = UIAlertAction(title: "라이브러리", style: UIAlertActionStyle.default) { action in
+            self.pickImageFromPhotoLibrary()
+        }
         let photoAlbumAction = UIAlertAction(title: "사진앨범", style: UIAlertActionStyle.default)
         let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.cancel) { action in
             self.dismiss(animated: true, completion: nil)
@@ -37,6 +39,15 @@ class ViewController: UIViewController {
         controller.addAction(cancelAction)
         
         self.present(controller, animated: true, completion: nil)
+    }
+    
+    func pickImageFromPhotoLibrary() {
+        let picker = UIImagePickerController()
+        
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false)
     }
     
 }
