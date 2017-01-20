625b37fc30656322d36c189a22c60f20411a3295
Jaemin <jaemin0216@gmail.com>
2017-01-20 14:37:23 +0900

    Implement delegate methods
    - imagePickerControllerDidCancel()
    - imagePickerController(_:didFinishPickingMediaWithInfo:)

--- Jaemin/170121/ImagePickerAssignment/ImagePickerAssignment/ViewController.swift
+++ Jaemin/170121/ImagePickerAssignment/ImagePickerAssignment/ViewController.swift
@@ -66,5 +66,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.present(picker, animated: false)
     }
     
+    // ImagePicker에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: false) {
+            // 알림창
+            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    // ImagePicker에서 이미지를 선택했을 때 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: false) { () in
+            // 선택된 이미지를 imageView에 표시
+            let image = info[UIImagePickerControllerEditedImage] as? UIImage
+            self.imageView.image = image
+        }
+    }
+    
 }
 
